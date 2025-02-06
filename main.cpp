ed063e39674c2f3019befc592e5415c9a452ff80
Bala Vinaithirthan <57514761+balavinaithirthan@users.noreply.github.com>
2025-02-06 15:07:02 -0800

    Basic example works

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
+#include <cstdio>
 #include <functional>
 #include <iostream>
 #include <mpi.h>
 #include <vector>
 
+enum {
+  RANK_0 = 0,
+};
+
+enum {
+  VEC_SIZE = 0,
+  VEC_DATA = 1,
+};
+
 std::vector<int> calculateSumVec(const std::vector<int> &vec) {
+  printf("calculating sum \n");
   std::vector<int> result(vec.size());
   result[0] = vec[0];
   for (int i = 1; i < vec.size(); i++) {
@@ -29,14 +40,14 @@ void redistribute() {}
 
 void send_out_work(int children_num, std::vector<int> parent_vec,
                    int redistribution_strategy) {
-  // include redistribution strategy
-  int chunk_size;
+  int chunk_size = parent_vec.size() / children_num;
   for (int i = 0; i < children_num; i++) {
-    MPI_Send(&chunk_size, 1, MPI_INT, i + 1, 0, MPI_COMM_WORLD);
-
     std::vector<int> chunk((parent_vec.begin() + (i * chunk_size)),
                            parent_vec.begin() + ((i + 1) * chunk_size));
-    MPI_Send(chunk.data(), chunk.size(), MPI_INT, i + 1, 1, MPI_COMM_WORLD);
+    MPI_Send(&chunk_size, 1, MPI_INT, i + 1, VEC_SIZE, MPI_COMM_WORLD);
+    MPI_Send(chunk.data(), chunk.size(), MPI_INT, i + 1, VEC_DATA,
+             MPI_COMM_WORLD);
+    std::cout << "parent sending out " << i << std::endl;
     debug_vector(chunk);
   }
 }
@@ -45,41 +56,43 @@ std::vector<int> combine_work(int children_num) {
   std::vector<int> sumVec;
   for (int childRank = 1; childRank < children_num + 1; childRank++) {
     int chunkSize;
-    MPI_Recv(&chunkSize, 1, MPI_INT, childRank, 2, MPI_COMM_WORLD,
+    MPI_Recv(&chunkSize, 1, MPI_INT, childRank, VEC_SIZE, MPI_COMM_WORLD,
              MPI_STATUS_IGNORE);
     std::vector<int> chunkVec(chunkSize);
-    MPI_Recv(chunkVec.data(), chunkSize, MPI_INT, childRank, 3, MPI_COMM_WORLD,
-             MPI_STATUS_IGNORE);
+    MPI_Recv(chunkVec.data(), chunkSize, MPI_INT, childRank, VEC_DATA,
+             MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     sumVec.insert(sumVec.end(), chunkVec.begin(), chunkVec.end());
+    std::cout << "parent receiving from " << childRank << std::endl;
+    debug_vector(chunkVec);
   }
   return sumVec;
 }
 
 void combineAndSendOutFn(const int children_num) {
-  // which children??
   auto combined = combine_work(children_num);
+  debug_vector(combined);
   flush();
   send_out_work(children_num, combined, 0);
-  // add flushes here
 }
 
-void chunk_children(
-    std::vector<std::function<std::vector<int>(std::vector<int>)>>
-        filter_list) {
+void children_code(
+    std::vector<std::function<std::vector<int>(const std::vector<int> &)>>
+        filter_list,
+    int rank) {
   int vec_size;
-  MPI_Recv(&vec_size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  std::vector<int> original_vector(vec_size);
-  MPI_Recv(original_vector.data(), vec_size, MPI_INT, 0, 1, MPI_COMM_WORLD,
+  MPI_Recv(&vec_size, 1, MPI_INT, RANK_0, VEC_SIZE, MPI_COMM_WORLD,
            MPI_STATUS_IGNORE);
-  std::vector<int> new_vec;
-  auto prevVec = std::move(original_vector);
-  for (const auto &filter : filter_list) {
-    new_vec = filter(prevVec);
-    prevVec = new_vec;
+  std::vector<int> original_vector(vec_size);
+  MPI_Recv(original_vector.data(), vec_size, MPI_INT, RANK_0, VEC_DATA,
+           MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  for (const auto &filterFun : filter_list) {
+    original_vector = filterFun(original_vector);
   }
-
-  MPI_Send(&vec_size, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
-  MPI_Send(new_vec.data(), new_vec.size(), MPI_INT, 0, 3, MPI_COMM_WORLD);
+  MPI_Send(&vec_size, 1, MPI_INT, RANK_0, VEC_SIZE, MPI_COMM_WORLD);
+  MPI_Send(original_vector.data(), original_vector.size(), MPI_INT, RANK_0,
+           VEC_DATA, MPI_COMM_WORLD);
+  std::cout << "child sending back " << std::endl;
+  debug_vector(original_vector);
 }
 
 int main(int argc, char **argv) {
@@ -88,51 +101,52 @@ int main(int argc, char **argv) {
   int rank, size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-  const int CHUNKS = 4;
+  const int CHUNKS = size - 1;
   if (size < CHUNKS + 1) {
     std::cerr << "This program requires at least " << CHUNKS + 1
               << " processes.\n ";
     MPI_Abort(MPI_COMM_WORLD, 1);
   }
-  std::vector<std::function<std::vector<int>(std::vector<int>)>> filter_list;
-  if (rank == 0) { // initial setup
-    filter_list.emplace_back(calculateSumVec);
-    // initialize filter list
+  std::vector<std::function<std::vector<int>(const std::vector<int> &)>>
+      filter_list;
+  filter_list.emplace_back(calculateSumVec);
+
+  if (rank == 0) {
     const int VEC_SIZE = 100;
-    std::vector<int> parent_vec;
+    std::vector<int> parent_vec(VEC_SIZE);
     for (int i = 0; i < VEC_SIZE; i++) {
-      parent_vec.push_back(i);
+      parent_vec[i] = i;
     }
-    int chunk_size = VEC_SIZE / CHUNKS; // TODO: assume perfectly divisible
-    for (int i = 0; i < CHUNKS; i++) {
-      std::vector<int> chunk((parent_vec.begin() + (i * chunk_size)),
-                             parent_vec.begin() + ((i + 1) * chunk_size));
-      MPI_Send(&chunk_size, 1, MPI_INT, i + 1, 0, MPI_COMM_WORLD);
-      MPI_Send(chunk.data(), chunk.size(), MPI_INT, i + 1, 1, MPI_COMM_WORLD);
-      debug_vector(chunk);
+    send_out_work(CHUNKS, parent_vec, 0);
+  }
+
+  if (filter_list.empty()) {
+    std::cerr << "Filter list is empty\n";
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+
+  int filter_count = filter_list.size();
+  while (true) {
+    if (filter_count == 1) {
+      break;
     }
+    if (rank != 0) {
+      children_code(filter_list, rank);
+    } else {
+      combineAndSendOutFn(CHUNKS);
+    }
+    filter_count--;
+    printf("filter count %d\n", filter_count);
   }
 
   if (rank != 0) {
-    std::vector<std::function<std::vector<int>(std::vector<int>)>> filter_slice(
-        filter_list.begin() + 1, filter_list.begin() + 4);
-    chunk_children(filter_slice);
+    children_code(filter_list, rank);
   } else {
-    combineAndSendOutFn(CHUNKS);
+    combine_work(CHUNKS);
   }
 
-  if (rank == 0) {
-    flush();
-    redistribute();
-  }
   MPI_Finalize();
   return 0;
 }
-// questions
-// should vector be copied over directly?
-// should vector be a shared data structure so that we can avoid copying?
-// only share numbers/view
 
-// layered distribution
-
-// mpic++ -std=c++11 -stdlib=libc++ -o hello_mpi main.cpp -v
+// mpirun -np 5 ./hello_mpi
